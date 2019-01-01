d60290ea53466d2f1243799498a2c5a54a620f5b
dundermifflin0 <dundermifflinresearch@gmail.com>
2019-01-01 09:02:24 +0000

    first commmit

---
+++ Negroni/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace Negroni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{nameof(JsonArrayAttribute)}");
+            //Console.WriteLine($"{nameof()}");
+        }
+    }
+}
